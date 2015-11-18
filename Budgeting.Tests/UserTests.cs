1c1951221f43154075ec8c9e85332f651ba2d8f6
Bhujan Sharma <bhujanv@proximity.bbdo.fr>
2015-11-18 17:39:17 +0400

    Added First Test Method

---
+++ Budgeting.Tests/UserTests.cs
@@ -0,0 +1,25 @@
+﻿using Budgeting.Core;
+using NUnit.Core;
+using NUnit.Framework;
+using System;
+
+namespace Budgeting.Tests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [Test]
+        [ExpectedException]
+        public void Username_Invalid_Isnull()
+        {
+            //Arrange
+            User user = new User("SampleUsername","123");
+            
+            //Act
+            Validator.checkUsernameIsNull(user.Username);
+
+            //Assert
+
+        }
+    }
+}
---
+++ Budgeting.Tests/Validator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Budgeting.Tests
+{
+    public class Validator
+    {
+        public static void checkUsernameIsNull(string username)
+        {
+            if (username.Equals(null) || username.Equals(""))
+            {
+                throw new Exception("Username cannot be null");
+            }
+        }
+
+    }
+}
