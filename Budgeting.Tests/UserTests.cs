08d14df524fee5a275417e251302b5ec7811a2a3
zuhayr <farreedunm@proximity.bbdo.fr>
2015-11-19 15:43:00 +0400

    UserService Implementation

--- Budgeting.Tests/UserTests.cs
+++ Budgeting.Tests/UserTests.cs
@@ -1,71 +1,95 @@
-﻿using Budgeting.Core;
+﻿using Budgeting.Core.Model;
+using Budgeting.Core;
+using Budgeting.Core.Services;
 using System;
 using NUnit.Framework;
+using Moq;
+using Budgeting.Core.Interfaces;
 
 namespace Budgeting.Tests
 {
     [TestFixture]
     public class UserTests
     {
+        private Mock<IUserRepository> mockRepository;
+
+        [TestFixtureSetUp]
+        public void Setup() 
+        {
+             mockRepository = new Mock<IUserRepository>();
+
+             mockRepository
+                 .Setup(t => t.Register(It.IsAny<string>(), It.IsAny<string>()))
+                 .Returns(() => new User(It.IsAny<string>(), It.IsAny<string>()));
+        }
+
         [Test]
-        [ExpectedException(typeof(Exception))]
-        public void Username_Invalid_Isnull()
+        [ExpectedException]
+        public void RegisterUser_Invalid_Isnull()
         {
             //Arrange
-            User user = new User("","123");
+            UserService userService = new UserService(mockRepository.Object);
+            User Result;
             
             //Act
-            Validator.checkUsernameIsNull(user.Username);
+            Result = userService.RegisterUser("", "123");
 
             //Assert
         }
 
         [Test]
-        public void Username_Invalid_IsNotNull()
+        public void RegisterUser_Valid_IsNotNull()
         {
+
             //Arrange
-            User user = new User("SampleName", "123");
+            UserService userService = new UserService(mockRepository.Object);
+            User Result;
 
             //Act
-            Validator.checkUsernameIsNull(user.Username);
+            Result = userService.RegisterUser("SampleName", "123");
 
             //Assert
+            Assert.IsNotNull(Result);
         }
 
-        [Test]
-        [ExpectedException(typeof(Exception))]
-        public void Password_Invalid_Isnull()
-        {
-            //Arrange
-            User user = new User("SamplePass", "");
+        //[Test]
+        //[ExpectedException(typeof(Exception))]
+        //public void Password_Invalid_Isnull()
+        //{
+        //    //Arrange
+        //    User user = new User("SamplePass", "");
 
-            //Act
-            Validator.checkPasswordIsNull(user.Password);
+        //    //Act
+        //    Validator.checkPasswordIsNull(user.Password);
 
-            //Assert
+        //    //Assert
 
-        }
+        //}
 
-        [Test]
-        public void Password_Valid_IsNotNull()
-        {
-            //Arrange
-            User user = new User("SamplePass", "pass");
+        //[Test]
+        //public void Password_Valid_IsNotNull()
+        //{
+        //    //Arrange
+        //    User user = new User("SamplePass", "pass");
 
-            //Act
-            Validator.checkPasswordIsNull(user.Password);
+        //    //Act
+        //    Validator.checkPasswordIsNull(user.Password);
 
-            //Assert
-        }
+        //    //Assert
+        //}
 
         [Test]
         public void RegisterUser_Valid_RegistrationShould()
         {
             //Arrange
+            
+            UserService userService = new UserService(mockRepository.Object);
 
             //Act
+            var Result = userService.RegisterUser("SampleUser", "123");
 
             //Assert
+            mockRepository.VerifyAll();
         }
     }
 }
