475f6ba8e1cf44f0bb79c8240bfbac30e52cdd3b
zuhayr <farreedunm@proximity.bbdo.fr>
2015-11-20 12:30:54 +0400

    saving changes

--- Budgeting.Core/Model/User.cs
+++ Budgeting.Core/Model/User.cs
@@ -14,7 +14,7 @@ namespace Budgeting.Core.Model
         public string Password { get; set; }
         public Guid id { get; set; }
 
-        public User(string username, string password)
+        public User(string username, string password);
 
         public User()
         {
@@ -23,8 +23,8 @@ namespace Budgeting.Core.Model
 
         public User(string Username, string Password)
         {
-            this.Username = username;
-            this.Password = password;
+            this.Username = Username;
+            this.Password = Password;
         }
     }
 
